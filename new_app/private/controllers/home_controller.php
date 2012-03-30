ab71a76b3e3a899df98fa2031e51d3fb756d9f78
Juan Pablo Marzetti <yonpols@gmail.com>
2012-03-30 19:17:06 -0300

    Initial documented samples

--- new_app/private/controllers/home_controller.php
+++ new_app/private/controllers/home_controller.php
@@ -1,9 +1,145 @@
 <?php
-    class HomeController extends Controller
-    {
-        public function index()
-        {
+    /**
+     * A controller is the glue element between your application presentation layer
+     * and your business logic and data (which resides in models)
+     *
+     * Each function defined in a controller is called an action. You can then
+     * set a route that calls an action of a controller (see routes.yml). In an
+     * action you will process user input, call models if you need and output data
+     * relevant to the presentation.
+     *
+     * If nothing specified (as usual) YPF will search a view file under views/.
+     * This file is expected to reside in a directory matching the name of the
+     * controller. For example, if you have a UsersController the path will be
+     * views/users/. If you have a namespace, for example Security\UsersController
+     * the path will be views/security/users/.
+     *
+     * The filename will be the underscore version of your action:
+     *  for login -> login.html
+     *  for resetPassword -> reset_password.html
+     *
+     * All variables set inside an action usign:
+     *  $this->data->var1  or the short ($this->var1)
+     *  $this->data->var2  or the short ($this->var2)
+     * will be available on your view as var1, var2
+     *
+     * You can also pass variables to your layout (it's an application shared
+     * html layout, also a view) by setting them to the $this->output object.
+     * In your layout view you can access this var using output.var1
+     *  $this->output->var1 = ...
+     *
+     * Properties available in the controller for use in an action:
+     *
+     * $this->name            name of the controller (including namespace)
+     * $this->application     instance of application object.
+     * $this->routes          instance of router. You can build routes by using
+     *                        $this->routes->route_name ->returns route url
+     *                        $this->routes->route_name(params) ->returns route
+     *                        url with params if route accepts any or with query
+     *                        params (the ones that goes after ? mark)
+     * $this->route           instance of the route that matched the request.
+     * $this->params          array of params available to the action. This array
+     *                        is a merge of GET and POST params and the extra params
+     *                        present in the route definition.
+     * $this->session         instance of session object. php's $_SESSION is not
+     *                        supported. You can set/get properties to session in the
+     *                        following way: $this->session->user_id = 1;
+     * $this->output          instance of output object. This object controls the way
+     *                        information is returned to the client and alse can be
+     *                        used to pass variables to layout.
+     *
+     * $this->output->title   title of the application being printed on HTML title tag
+     * $this->output->layout  name of layout to use when returning to the client
+     *                        (default: main)
+     * $this->output->format  format in which data will be presented to the client.
+     *                        This value is set by the framework according to the
+     *                        request unless it's explicitly set in a route definition
+     *                        or by changing it on an action. Possible values are:
+     *                        html (by default), clean (view without layout), json, xml
+     * $this->output->charset charset to be sent in header Content-Type (default utf-8)
+     *
+     * Methods available in the controller
+     *
+     * $this->error(string $message)
+     *      set /clear (when $message is null) an error message.
+     *      This value will be available on the layout as output.error
+     *
+     * $this->notice(string $message)
+     *      set /clear (when $message is null) a message.
+     *      This value will be available on the layout as output.notice
+     *
+     * $this->output(array $options)
+     *      set an output option (title, layout, format, charset). You can pass
+     *      several options as an associative array.
+     *
+     * $this->param(string $name, mixed $default = null)
+     * $this->p(string $name, mixed $default = null)
+     *      get a param from $this->params. If the parameter does not exists
+     *      default value will be returned. If no default value is passed, NULL
+     *      will be returned.
+     *
+     * $this->forwardTo(string $action, array $params = array())
+     *      forward execution loop to another action of this controller or other.
+     *      $action can be an action name, ex: 'createUser' which means call action
+     *      createUser of this controller, or 'invoices.new' which means call action
+     *      new of invoices.
+     *      aditionally you can add extra parameters that will be available on
+     *      $this->params.
+     *
+     * $this->redirectTo(string $url)
+     *      redirects client browser to the url passed.
+     *
+     * $this->render(string $template, array $options = array())
+     *      renders a view using the $template filename instead of the default
+     *      template name (the one that is calculated according to the roules
+     *      explained above). $options is an optional array that can contain
+     *      parameters to the output object (title, charset, format, layout) and
+     *      a special parameter partial. If $options['partial'] == true then
+     *      the view is rendered and text generated is returned by the render
+     *      function. Otherwise, control flow will jump to the application object
+     *      and the will be dispatched to the client.
+     *
+     * Events available in the controller
+     * You can attach several functions that will be called 'before', 'after' or
+     * 'on' an event. Each class can define it's own events. You can attach an
+     * event to a class using event handlers on the initialize static function.
+     *
+     * self::before('action', 'nameOfControllerMethod');
+     * This event is called before every action being called on a controller. You can
+     * attach several actions to an event. They will be called in the order they were
+     * attached. Inherited classes inherit parents attached events. If you want to clear
+     * the list you must call self::before('action', false); Action event send to the
+     * handler the action name being called. See for an example below.
+     *
+     * self::after('action', 'nameOfControllerMethod');
+     * This event is called after every action being called on a controller.
+     *
+     * See also:
+     *  models/sample.php, config.yml, views/home/index.html
+     *
+     */
+    class HomeController extends Controller {
+        /**
+         *  This function is called on every class when it is loaded. It is intended
+         *  to configure the class events and if you need to load settings.
+         *
+         *  Class events are available on every object descendant of YPFObject.
+         */
+        /*
+        public static function initialize() {
+            self::before('action', 'checkUser');
+        }
+        */
+
+        public function index() {
 
         }
+
+        /*
+        protected function checkUser($actionCalled) {
+            if ($actionCalled != 'index')
+                $this->redirectTo ($this->routes->login);
+        }
+        */
     }
 ?>
--- new_app/private/models/sample.php
+++ new_app/private/models/sample.php
@@ -40,35 +40,157 @@
          * Model admite tablas con claves compuestas.
          */
         public static $_tableName = 'samples';   //DEFECTO: nombre de la clase underscored.
-        public static $_keyFields = array('id'); //DEFECTO: array('id')
+        public static $_keyFields = array('id'); //DEFECTO: Se buscará en la estructura de la
+                                                 //tabla en la BD. Si no se define una clave,
+                                                 //se utilizará id.
 
-        //Alias a utilizar en la consulta SQL
-        public static $_aliasName = array('id'); //DEFECTO: null
+        /**
+         * Name of the database connection configured in current environment in
+         * config.yml file. By default this value is 'main'
+         * @var string 'main' if none specified.
+         */
+        public static $_databaseName = 'db2';
 
-        /* YPFRamework trabaja en UTF-8. Si se especifica un charset distinto
-         * Model convertirá los textos al cargarlos desde la base de datos y
-         * al guardarlos a la misma.*/
-        public static $_tableCharset = 'iso-8859-1'; //DEFECTO: utf-8
+        /**
+         * Table alias to use in sql queries.
+         * @var string null if none specified (no alias for SQL queries)
+         */
+        public static $_tableAlias = 'samples1'; //DEFECTO: null
 
-        /* Transient fields son campos que no se guardan en la base de datos pero
-         * están disponibles para su uso como campos comunes. Se inicializan con
-         * el valor indicado*/
+        /**
+         * Charset of data stored in the table. YPFramework works with UTF-8 by
+         * default. So if you specify a charset different from UTF-8, data loaded
+         * from de DB will be converted to UTF-8 and data loaded into DB will
+         * be converted to $_tableCharset.
+         *
+         * @var string 'utf-8' if none specified.
+         */
+        public static $_tableCharset = 'iso-8859-1';
+
+        /**
+         * Models define object properties automatically according to fields in the
+         * database. If you need properties that will not be stored in the database
+         * you need to define transient fields. You can also define variables in the
+         * model class.
+         *
+         * $_transientFields accepts an associative array whose keys are fields names
+         * and values are initial field values.
+         *
+         * @var array array() if not defined.
+         */
         public static $_transientFields = array(
             'name' => 'default value'
-        ); //DEFECTO: array()
+        );
 
-        /* Condiciones SQL: Las siguientes condiciones sirven para restringir el
-         * SELECT que se realiza a la BD a la hora de buscar registros.*/
 
-        //Campos a traer de la base de datos
-        public static $_sqlFields = array('id', 'name'); //DEFAULT: array();
-        //Joins a hacer 
-        public static $_sqlJoins = array('id', 'name');  //DEFAULT: array();
-        //Campos a traer de la base de datos
-        public static $_sqlFields = array('id', 'name'); //DEFAULT: array();
-        //Campos a traer de la base de datos
-        public static $_sqlFields = array('id', 'name'); //DEFAULT: array();
+        /**
+         * Array of fields to select. If this parameter is not defined all fields will
+         * be selected.
+         *
+         * @var array array('*') if none defined.
+         */
+        public static $_sqlFields = array('id', 'name');
 
+        /**
+         * Array of join clauses to add to SELECT.
+         *
+         * @var array array() if none defined.
+         */
+        public static $_sqlJoins = array('JOIN users ON users.id = samples1.user_id');
+
+        /**
+         * SQL conditions to specify in WHERE clause. This is an array
+         * of strings that will be concatenated with AND.
+         * @var array array() if none defined.
+         */
+        public static $_sqlConditions = array('samples1.active = 1', 'samples1.user_id IS NOT NULL');
+
+        /**
+         * GROUP BY fields. This is an array of strings that contain a field
+         * name each one.
+         *
+         * @var array array() if none defined.
+         */
+        public static $_sqlGrouping = array('samples1.date');
+
+        /**
+         * SQL conditions to specify in HAVING clause. This is an array
+         * of strings that will be concatenated with AND.
+         * @var array array() if none defined.
+         */
+        public static $_sqlGroupingConditions = array('COUNT(*) > 1');
+
+        /**
+         * ORDER BY fields. This is an array of strings that contain a field
+         * name each one and optionally a direction modifier (ASC or DESC)
+         *
+         * @var array array() if none defined.
+         */
+        public static $_sqlOrdering = array('date DESC');
+
+        /**
+         * LIMIT clause. This is an array of one or two integers that will be appended
+         * at the end of the SELECT sql query specifying offset and number of rows to
+         * load.
+         *
+         * @var array array() if none defined.
+         */
+        public static $_sqlLimit = array(20, 10);
+
+        /**
+         * List of named queries that can be applied on every query and subquery
+         * made to the model. This is an associative array of arrays. Each key will
+         * be the name of the query. Each array can specify sqlFields, sqlJoins,
+         * sqlConditions, sqlGruping, sqlGroupingConditions, sqlOrdering, sqlLimit
+         * to narrow the query.
+         *
+         * In the example below you can see two subqueries. They can be used as follows:
+         *
+         *  Samples::all()->active          -> list of all active samples
+         *  Samples::all()->active->top5    -> list of newest 5 active samples
+         *  Samples::where('user_id = ?', 5)->active ->list of all samples from
+         *                                     user#5 which are active.
+         *
+         * @var array array() if none defined.
+         */
+        public static $_queries = array(
+            'active' => array(
+                'sqlConditions' => array('active = 1')
+            ),
+            'top5' => array(
+                'sqlOrdering' => array('creation_date DESC'),
+                'sqlLimit' => array(0, 5)
+            )
+        );
+
+        /**
+         * List of relationships with this model. You can define four types
+         * of relation: belongs_to (n - 1), has_one (1 - 1), has_many (1 - n),
+         * has_many through (n - m)
+         *
+         * Relationship definition is an associative array of arrays with relation
+         * parameters.
+         *
+         * @var array() array() if none specified.
+         */
+
+        public static $_relations = array(
+            /**
+             * Belongs To relation.
+             *  'belongs_to' => 'ModelName'
+             *
+             * You must specify which foreign keys are present in this model
+             * that relate with primary keys of the related model.
+             *
+             * 
+             *
+             */
+            'sound_file' => array(
+                'belongs_to' => 'SoundFile',
+                'keys' => array('sound_file_id' => 'id'),
+                'keys' => array('sound_file_id'),
+            )
+        )
 
     }
 ?>
